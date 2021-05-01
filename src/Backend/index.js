75f6e9965815e01a4586906084aec7ede91a5c01
hea0516 <hea4152@gmail.com>
2021-05-01 16:51:29 +0800

    try push

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -3,22 +3,22 @@ const app = express();
 const cors = require('cors');
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://s1155126571:x73339@localhost/s1155126571');
 
 var db = mongoose.connection;
 // Upon connection failure
-db.on('error', console.error.bind(console,'Connection error:'));
+db.on('error', console.error.bind(console, 'Connection error:'));
 // Upon opening the database successfully
 db.once('open', function () {
 	console.log("Connection is open...");
 });
 
 var UserSchema = mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // NEED HASHING
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true }, // NEED HASHING
 	admin: { type: Boolean }
 });
 
@@ -39,26 +39,26 @@ var User = mongoose.model('User', UserSchema);
 /*var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}
 ));*/
-app.post('/login', function(req,res){ // LOGIN SYSTEM
-    console.log("Post request received!!");
-    var username = req.body.id;
-    var password = req.body.pw;
-    console.log(req.body);
-    console.log(req.body['id']);
-    User.findOne({username: username, password: password}, function(err,user){
-        if (err) {
-            return console.log('err'); // ERROR
-        }
+app.post('/login', function (req, res) { // LOGIN SYSTEM
+	console.log("Post request received!!");
+	var username = req.body.id;
+	var password = req.body.pw;
+	console.log(req.body);
+	console.log(req.body['id']);
+	User.findOne({ username: username, password: password }, function (err, user) {
+		if (err) {
+			return console.log('err'); // ERROR
+		}
 
-        if (!user) {
-            return res.json('FALSE'); // NO USER EXISTS. WRONG PW OR WRONG ID.
-        }
+		if (!user) {
+			return res.json('FALSE'); // NO USER EXISTS. WRONG PW OR WRONG ID.
+		}
 
-        if (user.admin == true) {
-            return res.status(201).json('ADMIN'); // ADMIN
-        }
-        else return res.status(200).json('TRUE'); // SUCCESSFUL
-    })
+		if (user.admin == true) {
+			return res.status(201).json('ADMIN'); // ADMIN
+		}
+		else return res.status(200).json('TRUE'); // SUCCESSFUL
+	})
 })
 
 //  SYNTAX FORMAT  //
@@ -103,4 +103,4 @@ app.post('/login', function(req,res){ // LOGIN SYSTEM
 */
 
 // listen to port 2096
-const server = app.listen(2101);
+const server = app.listen(2096);
