07569890f9c7f320fd96bdccc47f8f1f580b1746
hea0516 <hea4152@gmail.com>
2021-05-03 00:49:11 +0800

    Added favourite Place

--- src/Frontend/LoginedRouterClassUser.js
+++ src/Frontend/LoginedRouterClassUser.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import LongLink from "./LongLink.js";
 import Info from "./Info.js";
+import FavouritePlace from "./FavouritePlace.js";
 
 
 class LoginedRouterClassUser extends React.Component { //User Page
@@ -13,12 +14,14 @@ class LoginedRouterClassUser extends React.Component { //User Page
                     <h2>Welcome back, User! OwO</h2>
                     <ul>
                         <LongLink to="/info" label="Info" />
+                        <LongLink to="/favouriteplace" label="FavouritePlace" />
                     </ul>
 
                     <hr />
 
                     <Switch>
                         <Route path="/info" component={Info} />
+                        <Route path="/favouriteplace" component={FavouritePlace} />
                         {/*info is for testing only/}
             {/*<Route path="*" component={NoMatch} />*/}
                     </Switch>
